021a95fb9fbf443b8bccb5ab29a2b22f4b9c0393
Tuomas Välimaa <tuomas.valimaa@druid.fi>
2016-11-23 10:08:34 +0200

    First commmit - Alpha version

---
+++ src/Plugin/Filter/FilterMediatoken.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Drupal\mediatoken\Plugin\Filter;
+
+use Drupal\filter\FilterProcessResult;
+use Drupal\filter\Plugin\FilterBase;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Provides a filter to help celebrate good times!
+ *
+ * @Filter(
+ *   id = "mediatoken",
+ *   title = @Translation("Celebrate Filter"),
+ *   description = @Translation("Help this text format celebrate good times!"),
+ *   type = Drupal\filter\Plugin\FilterInterface::TYPE_MARKUP_LANGUAGE,
+ * )
+ */
+class FilterMediatoken extends FilterBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function process($text, $langcode) {
+    global $base_root;
+    $pattern = '
+      /
+      \{              # { character
+          (?:         # non-capturing group
+              [^{}]   # anything that is not a { or }
+              |       # OR
+              (?R)    # recurses the entire pattern
+          )*          # previous group zero or more times
+      \}              # } character
+      /x
+    ';
+    preg_match_all($pattern, $text, $matches);
+    $medias = $matches[0];
+    $imgs = [];
+    foreach($medias as $media => $element) {
+      $element = json_decode($element);
+      $db = \Drupal::database();
+      $query = $db->select('migrate_map_d7_file_managed', 'f');
+      $query->fields('f', ['filename']);
+      $query->condition('f.fid', $element->fid);
+      $data = $query->execute()->fetchCol();
+
+      $imgs[$media] = '<img class="media-element file-default" alt="'.$element->attributes->alt.'" title="'.$element->attributes->title.'" typeof="foaf:Image" src="'.$base_root.'/sites/default/files/d7/'.$data[0].'" width="'.$element->attributes->width.'" height="'.$element->attributes->height.'">';
+      $medias[$media] = '[['.$medias[$media].']]';
+    }
+
+    return new FilterProcessResult(str_replace($medias, $imgs, $text));
+  }
+}
